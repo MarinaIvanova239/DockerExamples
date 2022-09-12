ceb7c8de08c1123553cdbe025f9c646117a0973d
Marina.Ivanova <Ijyf23011996!>
2022-09-12 23:01:31 +0300

    fix sh scripts

--- postgresql/builld.sh
+++ postgresql/builld.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-docker pull postgres:latest
 docker build . --tag my_postgre_image
 docker run --name my_postgre -itd --publish 5432:5432 my_postgre_image
--- postgresql/clean.sh
+++ postgresql/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+docker stop my_postgre
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -a -q)
 docker volume rm $(docker volume ls -q)
